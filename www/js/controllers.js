6e283711a4f58a90b85b901925288a8b5adf651b
tinabitna <kangt0627@gmail.com>
2017-06-15 20:56:06 -0400

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('TasksCtrl', function($scope, Tasks) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.tasks = Tasks.all();
+  $scope.remove = function(task) {
+    Tasks.remove(task);
+  };
+})
+
+.controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
+  $scope.task = Tasks.get($stateParams.taskId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
